6e6fccd392ba018224cc50d9026ce766f7bab9fc
Thun (MacBook Pro) <bsthun@gmail.com>
2023-01-03 14:47:03 +0700

    Optimize spotify analytics duplicate key

--- backend/loaders/hub/spotify_helper.go
+++ backend/loaders/hub/spotify_helper.go
@@ -3,6 +3,7 @@ package hub
 import (
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"backend/loaders/mysql"
 	"backend/types/enum"
@@ -12,26 +13,19 @@ import (
 )
 
 func spotifyRecord(entity enum.SpotifyEntity, val string) {
-	var count uint64
 	date := text.GetDateString()
-	if result := mysql.DB.Model(new(model.DiarySpotifyAnalytics)).Select("COUNT(*)").Where("date = ? AND entity = ? AND value = ?", date, entity, val).First(&count); result.Error != nil {
-		logrus.Warnf("Unable to get spotify analytics (%s): %v", entity, result.Error)
-		return
-	}
-	if count == 0 {
-		if result := mysql.DB.Create(&model.DiarySpotifyAnalytics{
-			Date:   &date,
-			Entity: &entity,
-			Value:  &val,
-			Count:  value.Ptr[uint64](1),
-		}); result.Error != nil {
-			logrus.Warnf("Unable to create spotify analytics (%s): %v", entity, result.Error)
-			return
-		}
-	} else {
-		if result := mysql.DB.Model(new(model.DiarySpotifyAnalytics)).Where("date = ? AND entity = ? AND value = ?", date, entity, val).Update("count", gorm.Expr("count + ?", 1)); result.Error != nil {
-			logrus.Warn("Unable to update spotify analytics (%s): %v", entity, result.Error)
-			return
-		}
+	if result := mysql.DB.Clauses(clause.OnConflict{
+		DoUpdates: clause.Assignments(
+			map[string]any{
+				"count": gorm.Expr("count + 1"),
+			},
+		),
+	}).Create(&model.DiarySpotifyAnalytics{
+		Date:   &date,
+		Entity: &entity,
+		Value:  &val,
+		Count:  value.Ptr[uint64](1),
+	}); result.Error != nil {
+		logrus.Warnf("Unable to update spotify analytics (%s): %v", entity, result.Error)
 	}
 }
