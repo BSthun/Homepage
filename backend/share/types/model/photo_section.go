38c9ae5d65b4e196b13242918232a825b459b7b6
Thun (MacBook Pro) <bsthun@gmail.com>
2023-08-02 11:39:39 +0700

    Change database model

--- backend/share/types/model/photo_section.go
+++ backend/share/types/model/photo_section.go
@@ -7,7 +7,7 @@ type PhotoSection struct {
 	PhotoAlbum   *PhotoAlbum `gorm:"foreignKey:PhotoAlbumId" json:"photo_album"`
 	PhotoAlbumId *uint64     `gorm:"not null" json:"photo_album_id"`
 	Title        *string     `gorm:"type:VARCHAR(255); not null" json:"title"`
-	Subtitle     *string     `gorm:"type:VARCHAR(255); not null" json:"subtitle"`
+	Subtitle     *string     `gorm:"type:TEXT; not null" json:"subtitle"`
 	Path         *string     `gorm:"type:TEXT; not null" json:"path"`
 	Date         *time.Time  `gorm:"not null" json:"date"`
 	CreatedAt    *time.Time  `gorm:"not null"` // Embedded field
